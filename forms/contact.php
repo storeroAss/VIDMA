8a58c5cc1144d1f12af5bbe9b93fc946c941d8b0
Hernan Ramirez <toreromono@gmail.com>
2022-12-10 18:06:42 -0500

    Se elimino quienes somos

--- forms/contact.php
+++ forms/contact.php
@@ -5,16 +5,15 @@
     $mensaje = $_POST['message'];
     
     $emailTo ="toreromono@gmail.com";
+
     $asunto = "Contacto desde mi pagina Web";
     $txt = "De: $nombre \n";
     $txt .= "Correo: $mail \n";
-    $txt .= "Asunto: $telefono \n\n";
+    $txt .= "telefono: $telefono \n\n";
     $txt .= "Mensaje: $mensaje ";
 
 
     mail($emailTo, $asunto, $txt);
 
    header('Location:../index.html')
-  
-
 ?>
