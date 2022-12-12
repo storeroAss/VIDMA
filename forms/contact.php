edd1dc6cdfd28fd6c12c6c2d7863ff3889a301ce
Hernan Ramirez <toreromono@gmail.com>
2022-12-12 11:10:02 -0500

    se arreglo el favicons y el contac.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,23 @@
 <?php
-    $nombre = $_POST['name'];
-    $mail = $_POST['email'];
-    $telefono = $_POST['numero'];
-    $mensaje = $_POST['message'];
+    $nombre = $_POST['nombre'];
+    $cedula = $_POST['cedula'];
+    $telefono = $_POST['telefono'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
     
     $emailTo ="toreromono@gmail.com";
 
-    $asunto = "Contacto desde mi pagina Web";
+    $asunto = "Nuevo mensaje desde la Pagina de Vidma";
     $txt = "De: $nombre \n";
+    $txt .= "CC: $cedula \n";
     $txt .= "Correo: $mail \n";
     $txt .= "telefono: $telefono \n\n";
     $txt .= "Mensaje: $mensaje ";
 
 
     mail($emailTo, $asunto, $txt);
-
-   header('Location:../index.html')
-?>
+    
+    echo"<script>alert("Mensaje enviado Correctamente")</script>";
+    echo"<script> setTimeout(\"Location.href='../contac.html'\",1000)</script>;
+    
+    ?>
