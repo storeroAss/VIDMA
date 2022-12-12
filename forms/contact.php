942d5d356246287d1961a67681126554365549fe
Hernan Ramirez <toreromono@gmail.com>
2022-12-12 12:50:03 -0500

    correccion de php

--- forms/contact.php
+++ forms/contact.php
@@ -5,19 +5,16 @@
     $correo = $_POST['correo'];
     $mensaje = $_POST['mensaje'];
     
-    $emailTo ="toreromono@gmail.com";
+    $emailTo ="vidmaasesoria@gmail.com";
 
     $asunto = "Nuevo mensaje desde la Pagina de Vidma";
     $txt = "De: $nombre \n";
     $txt .= "CC: $cedula \n";
-    $txt .= "Correo: $mail \n";
+    $txt .= "Correo: $correo \n";
     $txt .= "telefono: $telefono \n\n";
     $txt .= "Mensaje: $mensaje ";
 
-
     mail($emailTo, $asunto, $txt);
-    
-    echo"<script>alert("Mensaje enviado Correctamente")</script>";
-    echo"<script> setTimeout(\"Location.href='../contac.html'\",1000)</script>;
+    header('Location:../contac.html');
     
     ?>
